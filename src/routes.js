4d4e953e0ca21195c45a2c51d5c094b57e76ad55
diondevara <42793740+diondevara@users.noreply.github.com>
2020-04-25 13:23:48 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,21 @@ import NotFoundView from './components/404.vue'
 
 // Import Views - Dash
 import DashboardView from './components/views/Dashboard.vue'
-import TablesView from './components/views/Tables.vue'
-import ExcelView from './components/views/Excel.vue'
 import TasksView from './components/views/Tasks.vue'
 import SettingView from './components/views/Setting.vue'
 import AccessView from './components/views/Access.vue'
 import ServerView from './components/views/Server.vue'
 import ReposView from './components/views/Repos.vue'
+import AspekView from './components/views/Aspek.vue'
+import capaianUnitView from './components/views/capaian-unit.vue'
+import dataDasarView from './components/views/datadasar.vue'
+import jenisSatkerView from './components/views/jenis-satker.vue'
+import periodeView from './components/views/periode.vue'
+import indikatorPeriodeView from './components/views/indikator-periode.vue'
+import masterIndikatorView from './components/views/master-indikator.vue'
+import satuanKerjaView from './components/views/satuan-kerja.vue'
+import indikatorSatuanKerjaView from './components/views/indikator-satuan-kerja.vue'
+import indikatorSatuanKerjaLogView from './components/views/indikator-satuan-kerja-log.vue'
 
 // Routes
 const routes = [
@@ -29,15 +37,56 @@ const routes = [
         name: 'Dashboard',
         meta: {description: 'Overview of environment'}
       }, {
-        path: 'tables',
-        component: TablesView,
-        name: 'Tables',
-        meta: {description: 'Simple and advance table in CoPilot'}
+        path: 'Aspek',
+        alias: '',
+        component: AspekView,
+        name: 'Aspek',
+        meta: {description: 'Aspek'}
       }, {
-        path: 'excel',
-        component: ExcelView,
-        name: 'Excel',
-        meta: {description: 'Simple and advance Excel in CoPilot'}
+        path: 'periode',
+        component: periodeView,
+        name: 'periode',
+        meta: {description: 'periode'}
+      }, {
+        path: 'satuan-kerja',
+        component: satuanKerjaView,
+        name: 'satuan-kerja',
+        meta: {description: 'satuan-kerja'}
+      }, {
+        path: 'indikator-satuan-kerja',
+        component: indikatorSatuanKerjaView,
+        name: 'indikator-satuan-kerja',
+        meta: {description: 'indikator-satuan-kerja'}
+      }, {
+        path: 'indikator-satuan-kerja-log',
+        component: indikatorSatuanKerjaLogView,
+        name: 'indikator-satuan-kerja-log',
+        meta: {description: 'indikator-satuan-kerja-log'}
+      }, {
+        path: 'indikator-periode',
+        component: indikatorPeriodeView,
+        name: 'indikator-periode',
+        meta: {description: 'indikator periode'}
+      }, {
+        path: 'master-indikator',
+        component: masterIndikatorView,
+        name: 'master-indikator',
+        meta: {description: 'master-indikator'}
+      }, {
+        path: 'capaian-unit',
+        component: capaianUnitView,
+        name: 'capaian-unit',
+        meta: {description: 'capaian-unit'}
+      }, {
+        path: 'jenis-satker',
+        component: jenisSatkerView,
+        name: 'jenis-satker',
+        meta: {description: 'jenis-satker'}
+      }, {
+        path: 'data-dasar',
+        component: dataDasarView,
+        name: 'data-dasar',
+        meta: {description: 'data-dasar unit'}
       }, {
         path: 'tasks',
         component: TasksView,
