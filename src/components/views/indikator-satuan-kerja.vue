2a34cbc9417a46c2b2e55baefe5eedf55a30739d
diondevara <42793740+diondevara@users.noreply.github.com>
2020-04-25 13:25:05 +0700

    Add files via upload

---
+++ src/components/views/indikator-satuan-kerja.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div id="app" ref="spreadsheet"></div>
+    <div>
+        <input type="button" value="Add New Row" @click="() => spreadsheet.insertRow()" />
+        <input type="button" value="Delete Selected Row" @click="() => spreadsheet.deleteRow()" />
+    </div>
+  </div>
+</template>
+
+<script>
+import jexcel from 'jexcel'
+import 'jexcel/dist/jexcel.css'
+import axios from 'axios'
+// var host = 'http://localhost:8015/'
+var host = 'http://localhost:8015/'
+export default {
+  // name: 'App',
+  data() {
+    return {
+      dataDasar: [],
+      form: {
+        nama: 'New Data'
+      }
+    }
+  },
+  mounted() {
+    this.load()
+  },
+  methods: {
+    load() {
+      axios.get(host + 'api/Indikator_SatuanKerja/').then(res => {
+        console.log(res.data)
+        var jexcelOptions = {
+          data: res.data,
+          allowToolbar: true,
+          onchange: this.updateRow,
+          oninsertrow: this.newRow,
+          ondeleterow: this.deleteRow,
+          responsive: true,
+          columns: [
+            { type: 'hidden', title: 'id', width: '10px' },
+            { type: 'text', title: 'id_indikator_periode', width: '120px' },
+            { type: 'text', title: 'id_satker', width: '120px' },
+            { type: 'text', title: 'bobot', width: '120px' },
+            { type: 'text', title: 'target', width: '120px' },
+            { type: 'text', title: 'capaian', width: '120px' },
+            { type: 'text', title: 'last_update', width: '120px' }
+          ]
+        }
+        let spreadsheet = jexcel(this.$el, jexcelOptions)
+        Object.assign(this, { spreadsheet })
+      })
+    },
+    newRow() {
+      axios.post(host + 'api/Indikator_SatuanKerja/', this.form).then(res => {
+        console.log(res.data)
+      })
+    },
+    updateRow(instance, cell, columns, row, value) {
+      axios.get(host + 'api/Indikator_SatuanKerja/').then(res => {
+        var index = Object.values(res.data[row])
+        index[columns] = value
+        console.log(index)
+        axios.put(host + 'api/Indikator_SatuanKerja/' + index[0], {
+          id: index[0],
+          id_indikator_periode: index[1],
+          id_satker: index[2],
+          bobot: index[3],
+          target: index[4],
+          capaian: index[5]
+        }).then(res => {
+          console.log(res.data)
+        })
+      })
+    },
+    deleteRow(instance, row) {
+      axios.get(host + 'api/Indikator_SatuanKerja/').then(res => {
+        var index = Object.values(res.data[row])
+        // console.log(index)
+        console.log(row)
+        axios.delete(host + 'api/Indikator_SatuanKerja/' + index[0])
+      })
+    }
+  }
+}
+</script>
